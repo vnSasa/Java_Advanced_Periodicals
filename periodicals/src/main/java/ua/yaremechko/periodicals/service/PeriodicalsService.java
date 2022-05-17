dc5cc895cb8c25444fbf10ee1d8c06a8f6a6e201
vnSasa <ol.yaremechko@gmail.com>
2022-05-17 10:39:24 +0300

    added logging role and style for login page

--- periodicals/src/main/java/ua/yaremechko/periodicals/service/PeriodicalsService.java
+++ periodicals/src/main/java/ua/yaremechko/periodicals/service/PeriodicalsService.java
@@ -2,6 +2,8 @@ package ua.yaremechko.periodicals.service;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,19 +12,24 @@ import ua.yaremechko.periodicals.domain.Periodical;
 
 @Service
 public class PeriodicalsService {
+	
+	private Logger logger = LoggerFactory.getLogger(PeriodicalsService.class);
 
 	@Autowired
 	private PeriodicalRepository periodicalRepository;
 
 	public Periodical save(Periodical periodical) {
+		logger.info("Create periodical item {} : " + periodical);
 		return periodicalRepository.save(periodical);
 	}
 
 	public List<Periodical> getAllPeriodicals() {
+		logger.info("Get all periodicals items");
 		return periodicalRepository.findAll();
 	}
 	
 	public Periodical findById(Integer id) {
+		logger.info("Get periodical item by id: " + id);
 		return periodicalRepository.findById(id).get();
 	}
 }
